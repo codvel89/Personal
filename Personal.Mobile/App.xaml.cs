b1b435091d1dd5d40ab47a2262b040fa90bf97de
Abner Velasco <codvel89@gmail.com>
2023-04-21 17:50:12 -0600

    initial

---
+++ Personal.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Personal.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Personal.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal.Mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
